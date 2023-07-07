1b2100e5016c882e8e4964378f81c8338df124e7
Jonathan Adly <gadly0123@gmail.com>
2023-07-07 07:57:05 -0400

    icd10 endpoint

--- app.py
+++ app.py
@@ -39,6 +39,35 @@ def home():
 
     return render_template("index.html")
 
+@app.route("/icd10", methods=["GET", "POST"])
+def icd10():
+    if request.method == "POST":
+        # get the comma separated icd10 codes and turn into a list, stripping all spaces and capitalizing each item
+        icd10_codes = [code.strip().upper() for code in request.form["icd10_codes"].split(",")]
+
+        base_url = "https://galenai.co/" 
+
+        # send question to GalenAI server
+        url = f"{base_url}/api/v1/create-bulk-icd10-streaming-channel/"
+        payload = {'codes': icd10_codes}
+        token = f"token 61cbb3ba57ef2e7d92bc1f1161a87fbd3922acca" #your token here
+        headers = {'Authorization': token, 'Content-Type': 'application/json'}
+        response = requests.post(url, json=payload, headers=headers)
+
+        # handle errors.
+        if response.status_code != 200:
+            return render_template("icd10.html", error=response.json()["detail"])
+        
+        # get a channel_name where generated text will be sent too as SSE (server sent events) as they become ready immmediately
+        # Each request is a new channel_name generated on the fly.
+        channel_name = response.json()["channel_name"]
+
+        # once the query is finished streaming, you can retrieve the full details via this query_id
+        # Save this in your DB to retrieve the full details later
+        query_id = response.json()["query_id"]
+        return render_template("listening_icd.html", channel_name=channel_name, query_id=query_id, base_url=base_url)
+
+    return render_template("icd10.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
