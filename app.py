b4c256b1b6bead621b92749e733c3062f336a2cf
Jonathan Adly <gadly0123@gmail.com>
2023-07-10 12:47:27 -0400

    stewardship

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         query = request.form["question"]  # get question from user
-        #base_url = "http://localhost:8000" 
+        base_url = "http://localhost:8000" 
         
-        base_url = "https://galenai.co/" # for production
+        #base_url = "https://galenai.co/" # for production
 
         # send question to GalenAI server
         url = f"{base_url}/api/v1/get-clinical-query-streaming-channel/" 
@@ -68,6 +68,46 @@ def icd10():
         return render_template("listening_icd.html", channel_name=channel_name, query_id=query_id, base_url=base_url)
 
     return render_template("icd10.html")
+
+@app.route("/stewardship", methods=["GET", "POST"])
+def stewardship():
+    if request.method == "POST":
+        base_url = "http://localhost:8000"
+        infection = request.form["infection"]  # get infection from user
+        age = int(request.form["age"]) if request.form['age'] else None  # get age from user
+        weight = int(request.form["weight"]) if request.form['weight'] else None # get weight from user
+        crcl = int(request.form["crcl"]) if request.form['crcl'] else None # get crcl from user
+  
+        liver_disease = False
+        if "liver_disease" in request.form:
+            liver_disease = True
+
+        # send question to GalenAI server
+        url = f"{base_url}/api/v1/create-stewardship-streaming-channel/"
+        payload = {"infection": infection}
+        if age:
+            payload["age"] = age
+        if weight:
+            payload["weight"] = weight
+        if crcl:
+            payload["crcl"] = crcl
+        if liver_disease:
+            payload["liver_disease"] = liver_disease
+
+        token = f"token 61cbb3ba57ef2e7d92bc1f1161a87fbd3922acca" #your token here
+
+        headers = {'Authorization': token, 'Content-Type': 'application/json'}
+        response = requests.post(url, json=payload, headers=headers)
+        if response.status_code != 200:
+            return render_template("stewardship.html", error=response.json()["detail"])
+
+        channel_name = response.json()["channel_name"]
+        query_id = response.json()["query_id"]
+        
+        return render_template("listening_stewardship.html", channel_name=channel_name, query_id=query_id, base_url=base_url)
+
+    return render_template("stewardship.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
