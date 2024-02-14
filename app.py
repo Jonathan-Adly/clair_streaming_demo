127cccd7041d6345dfce709e66641ac3b1407975
Jonathan Adly <gadly0123@gmail.com>
2024-02-14 11:53:47 -0500

    on label

--- app.py
+++ app.py
@@ -206,6 +206,49 @@ def byod():
     return render_template("byod.html")
 
 
+@app.route("/onlabel", methods=["GET", "POST"])
+def onlabel():
+    # you should upload a document via the GalenAI UI or the API first
+    if request.method == "POST":
+        query = request.form["question"]  # get question from user
+        drug_name = request.form["drug_name"]
+        collection_name = request.form["collection_name"]
+
+        # send question to GalenAI server
+        url = f"{BASE_URL}/api/v1/create-onlabel-streaming-channel/"
+        # it is best practice to always upload documents with a collection_name so you can easily retrieve them later
+        payload = {
+            "query": query,
+            "collection_name": collection_name,
+            "drug_name": drug_name,
+        }
+
+        token = f"token {TOKEN}"  # your token here
+        headers = {"Authorization": token, "Content-Type": "application/json"}
+        response = requests.post(url, json=payload, headers=headers)
+
+        # by default, GalenAI will not process queries outside of scope of the model.
+        if response.status_code != 200:
+            return render_template("onlabel.html", error=response.json()["detail"])
+
+        # get a channel_name where generated text will be sent too as SSE (server sent events) as they become ready immmediately
+        # Each request is a new channel_name generated on the fly.
+        channel_name = response.json()["channel_name"]
+
+        # once the query is finished streaming, you can retrieve the full details via this query_id
+        # Save this in your DB to retrieve the full details later
+        query_id = response.json()["query_id"]
+
+        # client will call ready to listen endpoint, then listen to the SSE as they become ready
+        return render_template(
+            "listening_onlabel.html",
+            channel_name=channel_name,
+            query_id=query_id,
+            base_url=BASE_URL,
+        )
+    return render_template("onlabel.html")
+
+
 @app.route("/med-review", methods=["GET", "POST"])
 def med_review():
     if request.method == "POST":
